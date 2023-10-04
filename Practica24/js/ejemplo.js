6f93988300e49f50a3d4d0e03f9b87e2d73dd395
JossCE <josselin.cruz@uabc.edu.mx>
2023-10-03 18:03:38 -0700

    Sintaxis basica de javascript, usando variables basicas y operaciones

---
+++ Practica24/js/ejemplo.js
@@ -0,0 +1,21 @@
+ var numero1 = prompt("Ingrese el primer numero");
+ var numero2 = prompt("Ingrese el segundo numero"); 
+
+var resultado = numero1 + numero2;
+
+alert(resultado);
+
+/*var mensaje = "Bienvenido a nuestro sitio web";
+var texto_1 = " texto comillas dobles, ";
+var texto_2 = " texto comillas simples...";
+
+alert (mensaje + texto_1 + texto_2);*/
+
+/*var clienteRegistrado = false;
+var ivaIncluido = true;
+
+alert (clienteRegistrado);*/
+
+var array = ['Tijuana', 'Rosarito','Ensenada','Mexicali'];
+alert (array);
+ 
