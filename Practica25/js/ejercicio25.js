55b500715adeb7c8ac98f9805c3fac23513d1fa1
JossCE <josselin.cruz@uabc.edu.mx>
2023-10-03 18:04:34 -0700

    Operaciones basicas con javaScript

---
+++ Practica25/js/ejercicio25.js
@@ -0,0 +1,11 @@
+var numero1 = prompt("Ingrese el primer numero:");
+var numero2 = prompt("Ingrese el segundo numero:"); 
+var num1 = parseInt(numero1);
+var num2 = parseInt(numero2);
+
+var suma = num1 + num2;
+var resta = num1 - num2;
+var multi = num1 * num2;
+var div = num1 / num2;
+
+alert("Resultado suma: " + suma + "\nResultado resta: " + resta + "\nResultado multiplicacion: " + multi + "\nResultado division: " + div);
