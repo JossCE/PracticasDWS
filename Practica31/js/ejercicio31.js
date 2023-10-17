a0f716d48f75bd776643f59aa670f4d9e900bf51
JossCE <josselin.cruz@uabc.edu.mx>
2023-10-16 19:09:48 -0700

    arreglos y propiedades de los arreglos y ciclos while y do while

---
+++ Practica31/js/ejercicio31.js
@@ -0,0 +1,31 @@
+//Ejercicio1
+dias_semana= ["Lunes","Martes","Miercoles","Jueves","Viernes"];
+
+//alert("Dia de la semana: " + dias_semana);
+
+//dias_semana.push("Sabado");
+//alert("Dias de la semana: " + dias_semana);
+
+dias_semana.splice(dias_semana.indexOf("Jueves"),1)
+alert("Dias de la semana: " + dias_semana);
+
+
+//Ejercicio 2
+/*var i = 0;
+var suma = 0;
+while(i < 6){
+    suma = i + suma;
+    i++;
+}
+alert(suma);*/
+
+
+//Ejercicio 3
+/*var i = 5;
+var total = 1;
+do{
+    total = total * i; 
+    i--;
+} while(i > 0);
+alert(total);*/
+
