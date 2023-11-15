aeec92f1bf33de94b3ef1d0bafd36824b8f7b877
JossCE <josselin.cruz@uabc.edu.mx>
2023-11-14 17:32:55 -0800

    Ejercicio crear formulario y guardar datos en base de datos

---
+++ EjemploBDD4/index.php
@@ -0,0 +1,43 @@
+<?php
+
+    $servidor = "localhost";
+    $usuario = "root";
+    $clave = "";
+    $bd = "registrocontactos";
+
+    $conexion = mysqli_connect ($servidor, $usuario, $clave, $bd);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro contacto</title>
+</head>
+<body>
+    <form action="#" name="registrocontactos" method="post">
+        <input type="text" name="Nombre" placeholder="nombre">
+        <input type="text" name="Telefono" placeholder="telefono">
+        <input type="text" name="Correo" placeholder="correo">
+        <input type="text" name="Comentarios" placeholder="comentarios">
+
+        <input type="submit" name="registro">
+    </form>
+</body>
+</html>
+
+<?php
+
+    if(isset($_POST['registro'])){
+        $nombre=$_POST['Nombre'];
+        $telefono=$_POST['Telefono'];
+        $correo=$_POST['Correo'];
+        $comentarios=$_POST['Comentarios'];
+
+        $guardar = "INSERT INTO contactos(Nombre,Telefono,Correo,Comentarios) VALUES ('$nombre','$telefono','$correo','$comentarios')";
+
+        $ejecutar = mysqli_query ($conexion, $guardar);
+    }
+?>
+
